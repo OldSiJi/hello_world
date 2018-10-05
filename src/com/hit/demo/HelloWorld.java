8a7c1484bc4679be5e11b95832521f2c4e5efbd2
itcast <bos@itcast.cn>
2018-10-05 15:36:35 +0800

    Initial commit

---
+++ src/com/hit/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.hit.demo;
+
+/**
+ * Created by 维C.大表哥 on 2018/10/5.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
